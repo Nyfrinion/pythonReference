02cb111a9a9bf40474cdc0ce239690f2887b5c42
nyfrinion <andrewocornish@gmail.com>
2024-10-08 12:46:09 -0600

    module

--- modules.py
+++ modules.py
@@ -1,5 +1,33 @@
 #MODULES
+#modules are files containing python codes; they are used to breakdown and organize code
+#often by use case
 
 #   Built-In Modules
+#python has a lot of built-in modules
+#common modules below
+
+#mathy
+math
+random
+statistics
+
+#file and os operations
+os
+shutil
+pathlib
+
+#networking
+socket
+http
+
+#date/time manipulation
+datetime
+time
+
+#data handling
+json
+csv
+pickle
 
 #   User-Defined Modules
+#see left as example
